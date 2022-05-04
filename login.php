985882307a3317818656759915038c5194e86c68
beckpalmx77 <beckpalmx@gmail.com>
2022-05-04 19:47:19 +0700

    ip

--- login.php
+++ login.php
@@ -99,7 +99,7 @@ include('includes/Header.php');
 <!-- Login Content -->
 <div class="container-login">
     <div class="row justify-content-center">
-        <div class="col-xl-6 col-lg-12 col-md-9">
+        <div class="col-xl-6 col-lg-6 col-md-9">
             <div class="card shadow-sm my-5">
                 <div class="card-body p-0">
                     <div class="row">
