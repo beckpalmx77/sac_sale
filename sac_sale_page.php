c8927f510fe7a7dbcc8f75e9fd40101f07de4113
beckpalmx77 <beckpalmx@gmail.com>
2023-04-22 14:04:10 +0700

    update

--- sac_sale_page.php
+++ sac_sale_page.php
@@ -372,6 +372,8 @@ if (strlen($_SESSION['alogin']) == "") {
             $('#product_name_detail').val($('#product_name').val());
             $('#price_detail').val($('#price').val());
 
+            $('#product_detail').val($('#product_id').val() + " | " + $('#product_name').val() + " | " + $('#price').val());
+
             let product_id_detail = $('#product_id').val();
 
             $('#TableStockList').DataTable().clear().destroy();
