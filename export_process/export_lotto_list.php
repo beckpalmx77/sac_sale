caa63fca2bdab007cf3e358bee72cc8d9c71b250
beckpalmx77 <beckpalmx@gmail.com>
2023-10-26 11:57:55 +0700

    update

--- export_process/export_lotto_list.php
+++ export_process/export_lotto_list.php
@@ -1,25 +1,26 @@
 <?php
 
+
 require_once '../config/connect_lotto_db.php';
 
-date_default_timezone_set('Asia/Bangkok');
+// Include XLSX generator library
+require_once '../vendor/PhpXlsxGenerator-master/PhpXlsxGenerator.php';
 
-$filename = "sac_lotto_list" . "-" . date('m/d/Y H:i:s', time()) . ".csv";
+// Excel file name for download
+$fileName = "sac_lotto_list" . "-" . date('m/d/Y H:i:s', time()) . ".xlsx";
 
 @header('Content-type: text/csv; charset=UTF-8');
 @header('Content-Encoding: UTF-8');
 @header("Content-Disposition: attachment; filename=" . $filename);
 
+// Define column names
+$excelData[] = array('ลำดับ', 'ชื่อร้าน', 'หมายเลขโทรศัพท์', 'จังหวัด', 'หมายเลขที่เลือก', 'ชื่อ Sale', 'วันที่บันทึก');
+
+// Fetch records from database and store in an array
+
 $select_query_daily = "  SELECT * FROM ims_lotto ORDER BY id ";
 
 $String_Sql = $select_query_daily ;
-
-/* $my_file = fopen("PGROUP.txt", "w") or die("Unable to open file!");
-fwrite($my_file,$String_Sql);
-fclose($my_file);
-*/
-
-$data = "ลำดับ,ชื่อร้าน,หมายเลขโทรศัพท์,จังหวัด,หมายเลขที่เลือก,ชื่อ Sale,วันที่บันทึก\n";
 
 $query = $conn->prepare($String_Sql);
 $query->execute();
@@ -29,21 +30,14 @@ if ($query->rowCount() >= 1) {
     $line_no = 0;
     foreach ($results as $result) {
         $line_no++;
-        $data .= " " . $line_no . ",";
-        $data .= " " . $result->lotto_name . ",";
-        $data .= " " . "[ " .$result->lotto_phone . " ]" . ",";
-        $data .= " " . $result->lotto_province . ",";
-        $data .= " " . $result->lotto_number . ",";
-        $data .= " " . $result->sale_name . ",";
-        $data .= " " . $result->create_date . "\n";
-
-        //$data .= str_replace(",", "^", $row['WL_CODE']) . "\n";
+        $lineData = array($line_no , $result->lotto_name, strval($result->lotto_phone), $result->lotto_province, strval($result->lotto_number), $result->sale_name, $result->create_date);
+        $excelData[] = $lineData;
     }
-
 }
 
-// $data = iconv("utf-8", "tis-620", $data);
-$data = iconv("utf-8", "windows-874", $data);
-echo $data;
+// Export data to excel and download as xlsx file
+$xlsx = CodexWorld\PhpXlsxGenerator::fromArray($excelData);
+$xlsx->downloadAs($fileName);
 
-exit();
+exit;
+
